c5f4c093b5c406773cfc37511c175210e7b2288a
Tobias L <toby.skov.ludvigsen@gmail.com>
2020-02-27 21:31:08 +0100

    Updates

---
+++ precache-manifest.8e98f1b6991d6f6358d966fd1eba87a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f7982fa90d220b288f03ed6b22dd10",
+    "url": "/WhiteAwayChallenge/index.html"
+  },
+  {
+    "revision": "de585b9c129505ddf6ed",
+    "url": "/WhiteAwayChallenge/static/css/main.e025e02e.chunk.css"
+  },
+  {
+    "revision": "a04519a85158ce3fd493",
+    "url": "/WhiteAwayChallenge/static/js/2.5f6caa3b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/WhiteAwayChallenge/static/js/2.5f6caa3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de585b9c129505ddf6ed",
+    "url": "/WhiteAwayChallenge/static/js/main.118e504e.chunk.js"
+  },
+  {
+    "revision": "ae0a117935153bedbd28",
+    "url": "/WhiteAwayChallenge/static/js/runtime-main.e3adf3e7.js"
+  },
+  {
+    "revision": "583ec1b69d2b79236d6b5113f9a5a5ce",
+    "url": "/WhiteAwayChallenge/static/media/logo.583ec1b6.png"
+  }
+]);
